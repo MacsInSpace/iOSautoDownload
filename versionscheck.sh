9cdaea2eb2d0f8b35c1703f4de5ccc2958e24622
Craig Hair <craig.hair@gmail.com>
2015-10-29 14:22:46 +1100

    Update versionscheck.sh

--- versionscheck.sh
+++ versionscheck.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
-URL="http://phobos.apple.com/versions"
-#while true; do
+if [ ! -d /Users/Shared/.iOS ]; then
+mkdir -p /Users/Shared/.iOS;
+fi;
+chmod -fR 777 /Users/Shared/.iOS
 
-if [[ -f new_versions.xml ]]; then
-  mv new_versions.xml old_versions.xml
+URL="http://phobos.apple.com/versions"
+wd="/Users/Shared/.iOS"
+cd $wd
+
+#while true; do
+if [ ! -d .new_versions.xml ]; then
+curl --proxy 10.xx.yy.zz:8080 -L -o .new_versions.xml -s $URL > /dev/null 2>&1;
+fi;
+
+if [[ -f .new_versions.xml ]]; then
+  mv .new_versions.xml .old_versions.xml
 fi
 
-curl --proxy 10.xx.yy.zz:8080 -L -o new_versions.xml -s $URL > /dev/null 2>&1
-diff ./new_versions.xml ./old_versions.xml > /dev/null
+curl --proxy 10.xx.yy.zz:8080 -L -o .new_versions.xml -s $URL > /dev/null 2>&1
+diff ./.new_versions.xml ./.old_versions.xml > /dev/null
 
 if [[ $? -ne 0 ]]; then
-  #run downloader
   echo "It changed"
   #run downloader
   date
