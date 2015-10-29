73dbff249fe8a16d2bbd5e80f47402a8570d7050
Craig Hair <craig.hair@gmail.com>
2015-10-29 14:24:49 +1100

    Update versionscheck.sh

--- versionscheck.sh
+++ versionscheck.sh
@@ -13,12 +13,16 @@ if [ ! -d .new_versions.xml ]; then
 curl --proxy 10.xx.yy.zz:8080 -L -o .new_versions.xml -s $URL > /dev/null 2>&1;
 fi;
 
+#add file size check and try another proxy
+
 if [[ -f .new_versions.xml ]]; then
   mv .new_versions.xml .old_versions.xml
 fi
 
 curl --proxy 10.xx.yy.zz:8080 -L -o .new_versions.xml -s $URL > /dev/null 2>&1
 diff ./.new_versions.xml ./.old_versions.xml > /dev/null
+
+#add file size check and try another proxy
 
 if [[ $? -ne 0 ]]; then
   echo "It changed"
