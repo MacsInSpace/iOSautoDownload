0db9674f7f55bd2abd382825861500db5e5f9979
Craig Hair <craig.hair@gmail.com>
2016-01-28 23:08:50 +1100

    Update versionscheck.sh

--- versionscheck.sh
+++ versionscheck.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-URL="http://phobos.apple.com/versions"
-wd="/Users/Shared/.iOS"
+URL="http://phobos.apple.com/version"
+wd="/Users/localadmin/Desktop/iOS"
+
+#wd="/Users/Shared/.iOS"
 
 if [ ! -d $wd ]; then
   mkdir -p $wd;
@@ -12,16 +14,25 @@ chmod -fR 777 $wd
 cd $wd
 
 #while true; do
-if [ ! -d .new_versions.xml ]; then
-  curl --proxy 10.xx.yy.zz:8080 -L -o .new_versions.xml -s $URL > /dev/null 2>&1;
+if [ ! -d new_versions.xml ]; then
+  #curl --proxy 10.xx.yy.zz:8080 -L -o .new_versions.xml -s $URL > /dev/null 2>&1;
+  curl -L -o new_versions.xml -s $URL > /dev/null 2>&1;
+  exit 0
 fi;
 
-if [[ -f .new_versions.xml ]]; then
-  mv .new_versions.xml .old_versions.xml
+#if [[ -f .new_versions.xml ]]; then
+ # mv .new_versions.xml .old_versions.xml
+#fi
+
+if [[ -f new_versions.xml ]]; then
+  mv new_versions.xml old_versions.xml
 fi
 
-curl --proxy 10.xx.yy.zz:8080 -L -o .new_versions.xml -s $URL > /dev/null 2>&1
-diff ./.new_versions.xml ./.old_versions.xml > /dev/null
+#curl --proxy 10.xx.yy.zz:8080 -L -o .new_versions.xml -s $URL > /dev/null 2>&1
+curl -L -o new_versions.xml -s $URL > /dev/null 2>&1;
+#curl -L -o ./.new_versions.xml -s $URL > /dev/null 2>&1;
+diff ./new_versions.xml ./old_versions.xml > /dev/null
+#diff ./.new_versions.xml ./.old_versions.xml > /dev/null 2>&1;
 
 #add file size check and try another proxy
 
